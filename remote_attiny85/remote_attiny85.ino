bf7afd8dae09fff72a358461b553a84d5d80e9ca
facelessloser <facelessloser@gmail.com>
2014-03-08 17:03:31 +0000

    now with added comments

--- remote_attiny85/remote_attiny85.ino
+++ remote_attiny85/remote_attiny85.ino
@@ -1,31 +1,31 @@
-#include <IRnew.h>
+#include <attiny85_RC6_IR.h>  // The modify IR libray
 
-const int buttonPin = 4;
-const int ledPin =  0;
+const int buttonPin = 4; // Button is on arduion pin 4 attiny pin 3
+const int ledPin =  0;  // Status LED on arduino pin 0 attiny85 pin 5
+                        // IR LED on arduino pin 1 attiny85 pin 6 this is defined in the IR libray
 
 int buttonPushCounter1 = 0;    // counts the button pushes
 int buttonState1 = 0;    // tracks the button state
 int lastButtonState1 = 0;    // last state of the button
 
-int buttonState = 0;
-int lastButtonState = LOW;
+int buttonState = 0;  // Button status
+int lastButtonState = LOW;  //Button set to LOW
 
-long a = 0;
-long b = 0;
-long c = 0;
+long a = 0;  // Keep track of how many time the channel code has been run
+long b = 0;  // Keep track of how many time the channel code has been run
+long c = 0;  // Keep track of how many time the channel code has been run
 
-IRsend irsend;
+IRsend irsend;  // Starts the IR code off
 
-// Debounce
-
-long lastDebounceTime = 0;
-long debounceDelay = 50;
+long lastDebounceTime = 0;  // Debounce status
+long debounceDelay = 50;  // Debounce timer
 
 void setup() {
-  pinMode(ledPin, OUTPUT);
-  pinMode(buttonPin, INPUT);
+  pinMode(ledPin, OUTPUT);  // Sets LED as output
+  pinMode(buttonPin, INPUT);  // Sets button as input
 }
 
+// Pre set channel 604 nickelodeon
 int remoteone() {
   a += 1;
   if (c == 1) {
@@ -45,6 +45,7 @@ int remoteone() {
   irsend.sendRC6(0xC00C83, 24);
 }
 
+// Pre set channel 604 nickelodeon
 int remotetwo() {
   b += 1;
   a -= 1;
@@ -62,6 +63,7 @@ int remotetwo() {
   irsend.sendRC6(0xC00C83, 24);
 }
 
+// Pre set channel 606 nickelodeon+1
 int remotethree() {
   c += 1;
   b -= 1;
@@ -82,7 +84,7 @@ int remotethree() {
 
   
 void loop() {
-    
+  // All the debound button press stuff
   int reading = digitalRead(buttonPin);
   buttonState1 = digitalRead(buttonPin);
   
@@ -111,7 +113,7 @@ void loop() {
   }
   lastButtonState = reading;
   
-  if (buttonPushCounter1 == 1) {
+  if (buttonPushCounter1 == 1) {  // Enumerate throught the button presses and only allowys the channel code to be run once
     if (a < 1) {
       remoteone();
     }
@@ -119,14 +121,14 @@ void loop() {
     }
   }
   
-  if (buttonPushCounter1 == 2) {
+  if (buttonPushCounter1 == 2) {  // Enumerate throught the button presses and only allowys the channel code to be run once
     if (b < 1) {
       remotetwo();
     }
     else {
   }
   }
-  if (buttonPushCounter1 == 3) {
+  if (buttonPushCounter1 == 3) {  // Enumerate throught the button presses and only allowys the channel code to be run once
     if (c < 1) {
       remotethree();
     }
